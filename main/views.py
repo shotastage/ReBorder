154ab62efb96361f9da8077fb43f27adf2f99609
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-23 00:18:41 +0900

    Add urls.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
+from django.views import View
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, 'pages/index.html')
+        else:
+            return render(request, 'pages/login.html')
