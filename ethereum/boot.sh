2dd2e5310d179dce7ef6148fe9a78e3564d491ba
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-25 12:32:59 +0900

    Add ethereum setup tools.

--- ethereum/boot.sh
+++ ethereum/boot.sh
@@ -4,4 +4,4 @@ if [[ $1="--init" ]]; then
     geth --datadir /home/ubuntu/rb_geth init /home/ubuntu/rb_geth/myGenesis.json
 fi
 
-geth --networkid "10" --nodiscover --datadir "/home/ubuntu/rb_geth" console 2>> /home/ubuntu/rb_geth/geth_err.log
+geth --networkid "10" --nodiscover --datadir "/home/ubuntu/rb_geth" 2>> /home/ubuntu/rb_geth/geth_err.log &
