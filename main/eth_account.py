b67daa28fa67c6048e8e8ee829603b2eca4fce0c
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-25 13:59:52 +0900

    Add npm related packages.

---
+++ main/eth_account.py
@@ -0,0 +1,18 @@
+class SFCoinAccount:
+    def get(self):
+        print("Hello, world!")
+
+    def detectSFCoinAcName(self, ethAccount):
+        # number = "0xf3c986c3d8e2481b3be06ec195beb665f578dd78"
+        n = []
+        initName = str(int(ethAccount, 16))
+
+        tmp = [(i+j) for (i,j) in zip(initName[::2],initName[1::2])]
+
+        for i in tmp:
+            n[i] = int(list(tmp[i])[0]) + int(list(tmp[i])[1])
+
+
+sfc = SFCoinAccount()
+
+print(sfc.detectSFCoinAcName("0xf3c986c3d8e2481b3be06ec195beb665f578dd78"))
