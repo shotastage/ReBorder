ad4068ab54ace1ef8596c473c5af9d367aa8a19f
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-25 22:59:31 +0900

    Add models.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user_first_name = models.CharField(max_length=100)
+    user_last_name = models.CharField(max_length=100)
+    user_student_id = models.CharField(max_length=8)
+    user_ethereum_account = models.CharField(max_length=100)
+    user_is_admin = models.BooleanField
+
+
+
+class GameSession(models.Model):
+    session_id = models.CharField(max_length=100)
+    isEnable = models.BooleanField
+    isRevoked = models.BooleanField
