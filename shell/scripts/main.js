8f1727e141c5188064164471db7ef16935b6de94
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-27 00:18:21 +0900

    Fix signup issue.

--- shell/scripts/main.js
+++ shell/scripts/main.js
@@ -6,13 +6,3 @@ signUpNextButton.addEventListener("click", function () {
     document.getElementById("signup_step1").classList.add("invisible");
     document.getElementById("signup_step2").classList.remove("invisible")
 });
-
-$("#reader").html5_qrcode(function(data){
- 		 // do something when code is read
- 	},
- 	function(error){
-		//show read errors
-	}, function(videoError){
-		//the video stream could be opened
-	}
-);
