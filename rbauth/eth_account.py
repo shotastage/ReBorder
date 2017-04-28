e82efda3fa97adb86aaf50b7b0c8bd6170e4a5a9
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-29 02:29:40 +0900

    Get eth account.

--- rbauth/eth_account.py
+++ rbauth/eth_account.py
@@ -9,4 +9,21 @@ class EthAccount():
         cmd.expect('Repeat passphrase:')
         cmd.sendline ("password")
         cmd.expect( pexpect.EOF )
+        output = str(cmd.before)
         cmd.close()
+
+
+        start_index = 0
+        end_index = 0
+
+        for char in output:
+            default_index = 0
+
+            if char == "{":
+                start_index = default_index
+            elif char == "}":
+                end_index = default_index
+
+            default_index = default_index + 1
+
+# b' \r\nAddress: {37b65d8968ce826663ed6f6f0ba94a32981639a6}\r\n'
