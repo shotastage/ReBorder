06c715b2e766c4ddb406a47dcd0adc40be04c339
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-29 01:26:08 +0900

    Complete eth account creater.

--- rbauth/eth_account.py
+++ rbauth/eth_account.py
@@ -3,12 +3,10 @@ import sys
 
 class EthAccount():
     def createAccount(self, password):
-        cmd = pexpect.spawn('geth account new')
-        cmd.expect('Passphrase: ')
-        cmd.sendline(password)
-        cmd.expect('Password: ')
-        cmd.sendline(password)
-        cmd.expect('Repeat passphrase: ')
-        cmd.sendline(password)
-        cmd.expect(pexpect.EOF)
+        cmd = pexpect.spawn ('geth account new')
+        cmd.expect ('Passphrase:')
+        cmd.sendline ("password")
+        cmd.expect('Repeat passphrase:')
+        cmd.sendline ("password")
+        cmd.expect( pexpect.EOF )
         cmd.close()
