9d8d32deb092601ceab852ea8455a56ad0f17e9a
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-29 02:52:17 +0900

    Get eth auth.

--- rbauth/eth_account.py
+++ rbauth/eth_account.py
@@ -15,15 +15,17 @@ class EthAccount():
 
         start_index = 0
         end_index = 0
+        default_index = 1
 
         for char in output:
-            default_index = 0
-
             if char == "{":
                 start_index = default_index
             elif char == "}":
-                end_index = default_index
-
+                end_index = default_index - 1
             default_index = default_index + 1
+
+
+        print(output[start_index:end_index])
+
 
 # b' \r\nAddress: {37b65d8968ce826663ed6f6f0ba94a32981639a6}\r\n'
