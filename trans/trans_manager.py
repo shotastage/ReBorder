ec963c2e131f962c82b61fe0472f751502a2edbd
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-30 00:30:56 +0900

    Add card registerer.

---
+++ trans/trans_manager.py
@@ -0,0 +1,17 @@
+from trans.models import Transaction
+from main.models import UserProfile
+
+class SFCoinTransaction():
+
+    @classmethod
+    def getEthAccount(self, username):
+        eth_account = UserProfile.objects.filter(user_ethereum_account=username)
+        eth = eth_account.user_ethereum_account
+        return eth
+
+    def registerFrom(self, req, username):
+        transaction = Transaction(
+            from_card = username,
+            from_eth_account = self.getEthAccount(username),
+        )
+        transaction.save()
--- trans/views.py
+++ trans/views.py
@@ -2,10 +2,12 @@ from django.views import View
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from main.card import CardManager
+from trans.trans_manager import SFCoinTransaction
 
 
 class Transfer(View):
     card = CardManager()
+    coin = SFCoinTransaction()
 
     def get(self, request):
         if request.user.is_authenticated:
@@ -21,3 +23,15 @@ class Transfer(View):
                 })
         else:
             return render(request, 'pages/login.html')
+
+    def post(self, request):
+        req_type = request.POST['post_type']
+
+        if req_type == "from":
+            self.coin.registerFrom(request, username)
+        return render(request, 'pages/transfer.html',
+                {
+                    'card_name': "card_user_name",
+                    'card_account_number': "card_account_number",
+                    'card_remaining_conins': "card_remaining_conins",
+                })
