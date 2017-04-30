f9d0ca5584d73daeffb4ac9b8400e6bef2e45d48
Shota Shimazu <hornet.live.mf@gmail.com>
2017-04-30 19:43:21 +0900

    Add basic charger.

--- charge/urls.py
+++ charge/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from charge.views import Charge
 
 
 urlpatterns = [
-    url(r'^', include('main.urls')),
-    url(r'^', include('rbauth.urls')),
-    url(r'^', include('trans.urls')),
-    url(r'^', include('charge.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^charge/', Charge.as_view(), name='charge'),
 ]
--- charge/views.py
+++ charge/views.py
@@ -1,3 +1,27 @@
+from django.views import View
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from main.card import CardManager
+from trans.trans_manager import SFCoinTransaction
 
-# Create your views here.
+
+class Charge(View):
+    card = CardManager()
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            card_user_name = self.card.getUserFullName(request)
+            card_account_number = self.card.getAccountNumber(request)
+            card_remaining_conins = self.card.getRemainingCoins(request)
+
+            return render(request, 'pages/charge.html',
+                {
+                    'card_name': card_user_name,
+                    'card_account_number': card_account_number,
+                    'card_remaining_conins': card_remaining_conins,
+                })
+        else:
+            return render(request, 'pages/login.html')
+
+    def post(self, request):
+        pass
